431fd67a4a05cb40be72e030616a3669e49863ee
Mo Zhu <mozhu888@gmail.com>
2016-10-18 22:16:27 -0400

    Revert "create writer model"
    This reverts commit a08b672a0eb858277fb558011275f892935fc554.

--- app/models/writer.rb
+++
@@ -1,7 +0,0 @@
-class Writer < ApplicationRecord
-  validates :display_name, presence: true, length: { maximum: 128 }
-  validates :full_name, presence: true, length: { maximum: 128 }
-  validates :password_digest, presence: true, length: { maximum: 128 }
-
-  has_secure_password
-end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161019013913) do
+ActiveRecord::Schema.define(version: 20161016191828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,14 +21,6 @@ ActiveRecord::Schema.define(version: 20161019013913) do
     t.text     "body",                   null: false
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
-  end
-
-  create_table "writers", force: :cascade do |t|
-    t.string   "display_name",    limit: 128, null: false
-    t.string   "full_name",       limit: 128, null: false
-    t.string   "password_digest", limit: 128, null: false
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
   end
 
 end
