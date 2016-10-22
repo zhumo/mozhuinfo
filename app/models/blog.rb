56b4dfad4b95313f54add731529c96f51cca11a9
Mo Zhu <mozhu888@gmail.com>
2016-10-22 16:36:05 -0400

    published and unpublished scope

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,6 +2,9 @@ class Blog < ApplicationRecord
   validates :title, presence: true, length: { maximum: 256 }
   validates :body, presence: true
 
+  scope :published -> { where.not(published_at: nil) }
+  scope :unpublished -> { where(published_at: nil) }
+
   def published?
     published_at.present?
   end
