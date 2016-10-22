ce91ec8008d86eecdba9ccb226cbd6b63c39b2ba
Mo Zhu <mozhu888@gmail.com>
2016-10-22 17:36:25 -0400

    add published and unpublished to blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,8 +2,8 @@ class Blog < ApplicationRecord
   validates :title, presence: true, length: { maximum: 256 }
   validates :body, presence: true
 
-  scope :published -> { where.not(published_at: nil) }
-  scope :unpublished -> { where(published_at: nil) }
+  scope :published, -> { where.not(published_at: nil) }
+  scope :unpublished, -> { where(published_at: nil) }
 
   def published?
     published_at.present?
