daa93f546b0ae53c8565eab950028ea550c8fbe2
Mo Zhu <mozhu888@gmail.com>
2017-02-04 21:14:32 -0500

    specs for blog model

---
+++ spec/models/blog_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Blog, type: :model do
+  describe 'validations' do
+    # Title
+    it { should validate_presence_of(:title) }
+    it { should validate_length_of(:title).is_at_most(256) }
+
+    # Body
+    it { should validate_presence_of(:body) }
+
+    # Summary
+    it { should validate_presence_of(:summary) }
+    it { should validate_length_of(:summary).is_at_most(256) }
+  end
+end
